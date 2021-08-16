66536bd2e46a592ae0363d1f648c4d2f625ca2d6
Leandro Iamele <leandroiamele@gmail.com>
2021-08-16 10:57:20 -0300

    add show action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index' # And remove the generated route `products/index`
-  resources :products, only: [:index, :new, :create]  
+  resources :products, only: [:index, :new, :create, :show]  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
